5e8c8ff73b5496115fd66eb5f1f20d1ee0aa9477
Onojeta Brown <onosbrown.saved@gmail.com>
2022-05-23 11:40:08 +0100

    more

---
+++ chapel.php
@@ -0,0 +1,420 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+  
+
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Adullam - Meet the provost</title>
+    <meta name="description" content="" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="media/favicon.png" />
+
+    <!-- Dependencies -->
+    <link
+      rel="stylesheet"
+      href="dependencies/bootstrap/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="dependencies/animate/animate.min.css" />
+    <link rel="stylesheet" href="dependencies/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="dependencies/flaticon/font/flaticon.css" />
+    <link rel="stylesheet" href="dependencies/slick/slick.css" />
+    <link
+      rel="stylesheet"
+      href="dependencies/magnific-popup/css/magnific-popup.css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/swiper/css/swiper.min.css"
+      type="text/css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/datetimepicker/jquery.datetimepicker.css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/select2/css/select2.min.css"
+      type="text/css"
+    />
+    <!-- Site Stylesheet -->
+    <link rel="stylesheet" href="assets/css/app.css" />
+    <!-- Google Web Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400&amp;display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400&amp;display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="https://browsehappy.com/">upgrade your browser</a> to improve
+        your experience and security.
+      </p>
+    <![endif]-->
+    <div id="preloader" class="preloader">
+      <div class="loaders loader-1">
+        <div class="loader-outter"></div>
+        <div class="loader-inner"></div>
+      </div>
+    </div>
+    <div id="wrapper" class="wrapper">
+      <!--=====================================-->
+      <!--=     		Header Start     		=-->
+      <!--=====================================-->
+   <?php include "submenu.php";?>
+      <!--=====================================-->
+      <!--=     	 	Header End      		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     		Meeting Start     		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=       	 Breadcrumb Start      	=-->
+      <!--=====================================-->
+    
+      <!--=====================================-->
+      <!--=     	 	Breadcrumb End     		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     	 	Profile Start     		=-->
+      <!--=====================================-->
+     <section class="about-wrap-style-6 motion-effects-wrap">
+        <div class="container">
+          <div class="section-heading style-12">
+            <div class="sub-title">Adullam- RCN Theology Seminary .</div>
+            <h2 class="title">
+         The Chapel.
+            </h2>
+          </div>
+          <div class="about-inner">
+            <ul class="shape-wrap">
+              <li
+                class="wow fadeInRightMedium animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  src="media/elements/element71.png"
+                  alt="Element"
+                  width="222"
+                  height="222"
+                />
+              </li>
+              <li
+                class="wow fadeInUp animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  class="motion-effects2"
+                  src="media/elements/element72.png"
+                  alt="Element"
+                  width="111"
+                  height="123"
+                />
+              </li>
+              <li
+                class="wow fadeInUpMedium animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  src="media/elements/element73.png"
+                  alt="Element"
+                  width="52"
+                  height="52"
+                />
+              </li>
+              <li
+                class="wow fadeInLeft animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  class="motion-effects4"
+                  src="media/elements/element74.png"
+                  alt="Element"
+                  width="111"
+                  height="123"
+                />
+              </li>
+              <li
+                class="wow fadeInRight animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  class="motion-effects5"
+                  src="media/elements/element75.png"
+                  alt="Element"
+                  width="176"
+                  height="176"
+                />
+              </li>
+              <li
+                class="wow fadeInDownMedium animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  src="media/elements/element76.png"
+                  alt="Element"
+                  width="58"
+                  height="59"
+                />
+              </li>
+              <li
+                class="wow fadeInRight animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  class="motion-effects5"
+                  src="media/elements/element77.png"
+                  alt="Element"
+                  width="203"
+                  height="202"
+                />
+              </li>
+              <li
+                class="wow fadeInLeft animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  class="motion-effects6"
+                  src="media/elements/element78.png"
+                  alt="Element"
+                  width="365"
+                  height="365"
+                />
+              </li>
+            </ul>
+            <ul class="figure-wrap">
+              <li
+                class="wow fadeInLeft animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.1s"
+              >
+                <img
+                  src="chapel/1.jpg"
+                  alt="About"
+                  width="400"
+                  height="300"
+                />
+              </li>
+              <li
+                class="wow fadeInUp animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.2s"
+              >
+                <img
+                  src="chapel/2.jpg"
+                  alt="About"
+                  width="300"
+                  height="390"
+                />
+              </li>
+              <li
+                class="wow fadeInRight animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.3s"
+              >
+                <img
+                  src="chapel/3.jpg"
+                  alt="About"
+                  width="420"
+                  height="210"
+                />
+              </li>
+              <li
+                class="wow fadeInUp animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.4s"
+              >
+                <img
+                  src="chapel/4.jpg"
+                  alt="About"
+                  width="416"
+                  height="300"
+                />
+              </li>
+              <li
+                class="wow fadeInLeft animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.5s"
+              >
+                <img
+                  src="chapel/5.jpg"
+                  alt="About"
+                  width="350"
+                  height="400"
+                />
+              </li>
+              <li
+                class="wow fadeInRight animated"
+                data-wow-duration="1s"
+                data-wow-delay="0.6s"
+              >
+                <img
+                  src="chapel/9.jpg"
+                  alt="About"
+                  width="360"
+                  height="300"
+                />
+              </li> 
+            </ul>
+          </div>
+        </div>
+      </section> <section class="about-wrap-style-3 motion-effects-wrap">
+        <div class="container isotope-wrap">
+          
+          <div class="row">  <div class="col-xl-6 col-12">
+              <div class="about-box style-3">
+                <div class="figure-box">
+                  <img
+                    src="chap2.png"
+                    alt="Baneer"
+                    width="930"
+                    height="815"
+                  />
+                </div>
+                <div class="shape-wrap">
+                  <img
+                    class="motion-effects5"
+                    src="media/elements/element68.png"
+                    alt="Element"
+                    width="228"
+                    height="228"
+                  />
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-6 col-12">
+              <div class="about-box style-3">
+                <ul class="block-list about-list">
+                
+                  <li>
+                    <br>  <br>  <br>  <br>
+                  <div class="content-box"> <br>  <br> 
+              <h2 class="title">Arome Iduh.</h2>
+              <div class="d-flex align-items-center mb-4">
+                <div class="designation"><strong>Chaplain (Adullam Theological Seminary)</strong> </div>
+                
+              </div>
+              <h6 class="description"> 
+RCN Theological Seminary (Adullam) is a Christ-centered community that commits itself to develop/grounding students biblically, spiritually, and physically. As a part of the rigorous training that goes on in the seminary, students attend chapel twice a week throughout the academic school year. 
+
+From powerful worship led by the Adullam music group to life-changing messages/sermons from renowned Christian ministers, the chapel provides a way for students, faculty, and staff to join together in unity of the faith to glorify and encounter God.
+              </h6>
+            
+              
+              <ul class="inline-list profile-social">
+                <li>
+                  <a href="#" class="facebook"
+                    ><i class="fab fa-facebook-f"></i
+                  ></a>
+                </li>
+                <li>
+                  <a href="#" class="twitter"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                  <a href="#" class="instagram"
+                    ><i class="fab fa-instagram"></i
+                  ></a>
+                </li>
+               
+                <li>
+                  <a href="#" class="youtube"><i class="fab fa-youtube"></i></a>
+                </li>
+              </ul>
+            </div>
+                  </li>
+                 
+                </ul>
+              </div>
+            </div>
+         
+          </div>
+        </div>
+      </section>
+
+      <!--=====================================-->
+      <!--=     	 	Profile End     		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     	 	 Meeting End      		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     	 	Footer Start      		=-->
+      <!--=====================================-->
+        <?php include "footer.php";?>
+      <!--=====================================-->
+      <!--=     	 	Footer End      		=-->
+      <!--=====================================-->
+    </div>
+    <!-- Modal Start -->
+    <!-- <div class="modal fade" id="myModal" tabindex="-1" aria-hidden="true"> 
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+                </div>
+                <div class="modal-body">
+                    <div class="content-box">
+                        <div class="sub-title">Get Our New Free Course</div>
+                        <h2 class="title">Awesome for Website</h2>
+                        <form>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail address">
+                            <input type="submit" class="submit-btn" name="subscribe-btn" value="DOWNLOAD NOW">
+                        </form>
+                        <p class="description"><i class="fas fa-lock"></i>Your infomation will never be shared with any third party</p>
+                    </div>
+                    <div class="figure-box">
+                        <img src="media/banner/banner14.png" alt="Banner" width="298" height="543">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- Modal End -->
+
+    <script src="dependencies/jquery/jquery.min.js"></script>
+    <script src="dependencies/popper/popper.min.js"></script>
+    <script src="dependencies/bootstrap/js/bootstrap.min.js"></script>
+    <script src="dependencies/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="dependencies/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="dependencies/parallaxie/parallaxie.js"></script>
+    <script src="dependencies/slick/slick.min.js"></script>
+    <script src="dependencies/magnific-popup/js/jquery.magnific-popup.min.js"></script>
+    <script src="dependencies/waypoints/jquery.waypoints.min.js"></script>
+    <script src="dependencies/counter-up/jquery.counterup.min.js"></script>
+    <script src="dependencies/countdown/jquery.countdown.min.js"></script>
+    <script src="dependencies/wow/js/wow.min.js"></script>
+    <script src="dependencies/swiper/js/swiper.min.js"></script>
+    <script src="dependencies/validator/validator.min.js"></script>
+    <script src="dependencies/gmap3/gmap3.min.js"></script>
+    <script src="dependencies/tween-max/tween-max.min.js"></script>
+    <script src="dependencies/datetimepicker/jquery.datetimepicker.full.min.js"></script>
+    <script src="dependencies/select2/js/select2.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB13ZAvCezMx5TETYIiGlzVIq65Mc2FG5g"></script>
+
+    <!-- Site Scripts -->
+    <script src="assets/js/app.js"></script>
+  </body>
+
+</html>
