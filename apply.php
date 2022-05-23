8f0fdc294225cdc15c26517fb686d6a619c25317
Onojeta Brown <onosbrown.saved@gmail.com>
2022-05-23 11:30:10 +0100

    more

---
+++ apply.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html class="no-js" lang="">
+  
+
+<head>
+    <!-- Meta Data -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Adullam -contact us |RCN Bible Seminary</title>
+    <meta name="description" content="" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="media/favicon.png" />
+
+    <!-- Dependencies -->
+    <link
+      rel="stylesheet"
+      href="dependencies/bootstrap/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="dependencies/animate/animate.min.css" />
+    <link rel="stylesheet" href="dependencies/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="dependencies/flaticon/font/flaticon.css" />
+    <link rel="stylesheet" href="dependencies/slick/slick.css" />
+    <link
+      rel="stylesheet"
+      href="dependencies/magnific-popup/css/magnific-popup.css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/swiper/css/swiper.min.css"
+      type="text/css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/datetimepicker/jquery.datetimepicker.css"
+    />
+    <link
+      rel="stylesheet"
+      href="dependencies/select2/css/select2.min.css"
+      type="text/css"
+    />
+    <!-- Site Stylesheet -->
+    <link rel="stylesheet" href="assets/css/app.css" />
+    <!-- Google Web Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400&amp;display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400&amp;display=swap"
+      rel="stylesheet"
+    />
+  </head>
+
+  <body>
+    <!--[if lte IE 9]>
+      <p class="browserupgrade">
+        You are using an <strong>outdated</strong> browser. Please
+        <a href="https://browsehappy.com/">upgrade your browser</a> to improve
+        your experience and security.
+      </p>
+    <![endif]-->
+    <div id="preloader" class="preloader">
+      <div class="loaders loader-1">
+        <div class="loader-outter"></div>
+        <div class="loader-inner"></div>
+      </div>
+    </div>
+    <div id="wrapper" class="wrapper">
+      <!--=====================================-->
+      <!--=     		Header Start     		=-->
+      <!--=====================================-->
+       <?php include "submenu.php";?>
+      <!--=====================================-->
+      <!--=     	 	Header End      		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=       	 Breadcrumb Start      	=-->
+      <!--=====================================-->
+      <div
+        class="breadcrumb-wrap-style-2"
+        data-bg-image="media/banner/banner12.jpg"
+      >
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Contact Us
+            </li>
+          </ol>
+        </nav>
+       
+      </div>
+      <!--=====================================-->
+      <!--=     	 	Breadcrumb End     		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     	 	Contact Start     		=-->
+      <!--=====================================-->
+      <section class="contact-wrap-style-1">
+        <div class="container">
+       
+          <div class="contact-form style-1">
+            <div class="inner">
+              <h2 class="title">Adullam Diploma in Theology & Ministry June 2022 Application.</h2>
+              <p class="description">
+               The RCN Bible Seminary - Adullam, offers a Diploma Program in Theology and Ministry.
+
+A training program that is both spiritually imparting and intellectual.<hr width="100%" height="5px">
+              </p>
+           <h2 class="title">   <form id="contact-form"
+                <div class="row">
+                  <div class="col-md-6 form-group">
+                    <input 
+                      type="text" 
+                      placeholder="Title"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="Last Name "
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				 <hr width="100%" height="2px">
+				  
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="First Name"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div><div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="Date of Birth"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				    
+				 <hr width="100%" height="5px">
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="Contact Address"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div><div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="City "
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				  <hr width="100%" height="5px">
+				  
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="State/Province"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				   <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="Country"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  <hr width="100%" height="5px">
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="text"
+                      placeholder="Postal Code/Zip"
+                      class="form-control"
+                      name="name"
+                      data-error="Name field is required"
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Telephone (Preferably WhatsApp Contact) "
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div><div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Telephone (Preferably WhatsApp Contact) "
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				  <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Profession/Training "
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				   
+				  <hr width="100%" height="5px">
+				  
+				  
+				   <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="First Referee's Name "
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+              
+                  </div>
+				  
+				   <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Referee's Phone Number"
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+                 
+                  </div>
+				  
+				   <hr width="100%" height="5px">
+				  
+				  
+				   <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Second Referee's Name "
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+                 
+                  </div>
+				  
+				   <div class="col-md-6 form-group">
+                    <input
+                      type="email"
+                      placeholder="Referee's Phone Number"
+                      class="form-control"
+                      name="email"
+                      
+                      required
+                    />
+       
+                  </div>
+				 
+				   <style>div.b {
+  text-align: left;
+}
+
+</style><div class="b">
+<div class="col-md-12 form-group">
+                    <label for="cars">Gender</label>
+
+<select name="cars" class="description"   >
+
+  <option value="volvo">Male</option>
+  <option value="saab">Female</option>
+ 
+</select>
+                    <div class="help-block with-errors"></div>
+                  </div>
+	<div class="col-md-12 form-group">
+                    <label for="cars">Are you Born Again?</label>
+
+<select name="cars" class="description"   >
+
+  <option value="volvo">Yes</option>
+  <option value="saab">No</option>
+ 
+</select>
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				  
+				<div class="col-md-12 form-group">
+                    <label for="cars">Learning Option</label>
+
+<select name="cars" class="description"   >
+
+  <option value="volvo">On-Campus (the Diploma Program)</option>
+  <option value="saab">Online (Certificate of Participation)</option>
+ 
+</select>
+                    <div class="help-block with-errors"></div>
+                  </div>
+				  
+				  </div>
+				
+                  <button type="submit" class="btn-fill style-1">
+                     Proceed with Application
+                    </button>
+              
+                </div>  </h2>
+                <div class="form-response"></div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--=====================================-->
+      <!--=     	 	Contact End     		=-->
+      <!--=====================================-->
+      <!--=====================================-->
+      <!--=     	 	Footer Start      		=-->
+      <!--=====================================-->
+        <?php include "footer.php";?>
+      <!--=====================================-->
+      <!--=     	 	Footer End      		=-->
+      <!--=====================================-->
+    </div>
+    <!-- Modal Start -->
+    <!-- <div class="modal fade" id="myModal" tabindex="-1" aria-hidden="true"> 
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+                </div>
+                <div class="modal-body">
+                    <div class="content-box">
+                        <div class="sub-title">Get Our New Free Course</div>
+                        <h2 class="title">Awesome for Website</h2>
+                        <form>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail address">
+                            <input type="submit" class="submit-btn" name="subscribe-btn" value="DOWNLOAD NOW">
+                        </form>
+                        <p class="description"><i class="fas fa-lock"></i>Your infomation will never be shared with any third party</p>
+                    </div>
+                    <div class="figure-box">
+                        <img src="media/banner/banner14.png" alt="Banner" width="298" height="543">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- Modal End -->
+
+    <script src="dependencies/jquery/jquery.min.js"></script>
+    <script src="dependencies/popper/popper.min.js"></script>
+    <script src="dependencies/bootstrap/js/bootstrap.min.js"></script>
+    <script src="dependencies/imagesloaded/imagesloaded.pkgd.min.js"></script>
+    <script src="dependencies/isotope-layout/isotope.pkgd.min.js"></script>
+    <script src="dependencies/parallaxie/parallaxie.js"></script>
+    <script src="dependencies/slick/slick.min.js"></script>
+    <script src="dependencies/magnific-popup/js/jquery.magnific-popup.min.js"></script>
+    <script src="dependencies/waypoints/jquery.waypoints.min.js"></script>
+    <script src="dependencies/counter-up/jquery.counterup.min.js"></script>
+    <script src="dependencies/countdown/jquery.countdown.min.js"></script>
+    <script src="dependencies/wow/js/wow.min.js"></script>
+    <script src="dependencies/swiper/js/swiper.min.js"></script>
+    <script src="dependencies/validator/validator.min.js"></script>
+    <script src="dependencies/gmap3/gmap3.min.js"></script>
+    <script src="dependencies/tween-max/tween-max.min.js"></script>
+    <script src="dependencies/datetimepicker/jquery.datetimepicker.full.min.js"></script>
+    <script src="dependencies/select2/js/select2.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB13ZAvCezMx5TETYIiGlzVIq65Mc2FG5g"></script>
+
+    <!-- Site Scripts -->
+    <script src="assets/js/app.js"></script>
+  </body>
+
+
+</html>
